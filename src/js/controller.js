d006dda6f845183b0cfc29460b4a0688ba35b377
szpytma <szpytmapawel@hotmail.com>
2023-01-16 12:25:52 +0000

    developed DOM update algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,6 +19,9 @@ const controlRecipe = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0. Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // 1. Loading recipe
     await model.loadRecipe(id);
 
@@ -61,7 +64,8 @@ const controlServings = function (newServings) {
   // Update the recipe servings(in state).
   model.updateServings(newServings);
   // Update the view.
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
